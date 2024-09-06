5f5ee1321efa5885ac227e5001eb5c13067c52e9
Aadrij Upadya <adrij2005@gmail.com>
2024-09-06 15:49:09 -0700

    Register page styling finished, validation next

--- main.py
+++ main.py
@@ -14,33 +14,16 @@ app.secret_key = "cclquizgame"
 @app.route('/', methods = ['GET','POST'])
 
 def home():
-    # return render_template("./pages/index.html")
     return render_template('index.html')
     
 
-# @app.route('/pages/about', methods=['GET', 'POST'])
-# def about():
-#     return render_template("./pages/about.html")
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template("about.html")
     
-# @app.route("./register", methods=["GET", "POST"])
-# def register():
-#     if request.method == "GET":
-#         if 'registered' in session:
-#             print("you've been redirected")
-#             return redirect(url_for("login"))
-#     elif request.method == "POST":
-#         first = request.form["first"]
-#         last = request.form["last"]
-#         email = request.form["email"]
-#         password = request.form["password"]
-#         cursor = mysql.connection.cursor()
-#         cursor.execute(''' INSERT INTO user_info(first, last, gmail, password) VALUES(%s, %s, %s, %s) ''',
-#                        (first, last, email, password))
-#         mysql.connection.commit()
-#         session["registered"] = True
-#         print("in session")
-#         return redirect(url_for("login"))
-#     return render_template("register.html")
+@app.route("/register", methods=["GET", "POST"])
+def login():
+    return render_template("register.html")
     
 
 if __name__ == "__main__":
